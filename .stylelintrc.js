58d07d18851f5232797e550c8678c1f9a5468bac
Eliaztray <944126009@qq.com>
2019-12-03 11:12:48 +0800

    feat: complete stylelint config

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": [
+    // 这里添加对应的 预处理语言的样式规则，
+    // sass-recommend or others
+    "prettier"
+  ],
+  "plugins": ["stylelint-prettier"],
+  "rules": {
+    "prettier/prettier": true
+  }
+}
